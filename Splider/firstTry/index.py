769c461a78114dbf2009da4a386f4a79664c2c78
timeface <timeface@timefacedeiMac.local>
2018-11-06 20:47:43 +0800

    分析html的结构，是不是正则的问题

--- Splider/firstTry/index.py
+++ Splider/firstTry/index.py
@@ -23,3 +23,12 @@ def craw(url, page):
 if __name__ == '__main__':
     url = "https://list.jd.com/list.html?cat=9987,653,655&page=1&sort=sort_rank_asc&trans=1&JL=6_0_0#J_main"
     craw(url, 1)
+
+name = """
+    //img30.360buyimg.com/jgsq-productsoa/jfs/t27292/168/1057078522/1632/ae3f0411/5bc042fbN42f84c5e.png",sizingMethod="noscale");\'></div>            
+    </div>\n            <div class="p-scroll">\n              <span class="ps-prev"> < </span>\n              <span class="ps-next"> > 
+    </span>\n              <div class="ps-wrap">\n                <ul class="ps-main">\n                  
+    <li class="ps-item" ids=""> <a title="\xe9\x93\x82\xe5\x85\x89\xe9\x87\x91" 
+    href="javascript:;" ><img  data-sku="6703015" width="25" height="25" 
+    class="loading-style2" data-lazy-img="//img10.360buyimg.com/n9/jfs/t20305/259/1209609364/193755/a3940552/5b21ce25N131ce626.jpgjpg
+"""
